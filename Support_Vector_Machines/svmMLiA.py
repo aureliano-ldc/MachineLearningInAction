9d803628909e86156218127b2aa388c28748f9fc
decong.liu <912547365@qq.com>
2017-10-23 23:54:28 +0800

    fistly add SVM

---
+++ Support_Vector_Machines/svmMLiA.py
@@ -0,0 +1,10 @@
+
+
+
+"""
+间隔（margin）：训练数据集中所有样本点与分离超平面距离最小值
+支持向量（support vector）：与分离超平面距离最近的样本点实例
+We’d like to find the point closest to the separating hyperplane and make sure 
+this is as far away from the separating line as possible. This is known as margin.
+The points closest to the separating hyperplane are known as support vectors.
+"""
